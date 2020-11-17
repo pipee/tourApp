46bde592226516600cab86f0b88132bbd72c5514
Luis Felipe Armijo <luis.armijo@axity.com>
2020-11-16 21:50:38 -0300

    swagger

--- src/main/java/com/escalab/SwaggerConfig.java
+++ src/main/java/com/escalab/SwaggerConfig.java
@@ -1,4 +1,28 @@
 package com.escalab;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+
+    public static final Contact DEFAULT_CONTACT = new Contact("Escalab", "https:www.laescala.cl","");
+
+    public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Media Api Documentation","TourApp Api Documentation", "1.0",
+            "PREMIUM", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-20",
+            new ArrayList<VendorExtension>());
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO);
+    }
 }
