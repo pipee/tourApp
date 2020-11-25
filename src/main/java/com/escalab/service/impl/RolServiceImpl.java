6e65dba494974fc7b8b5256fd4f1ac743356b22c
Luis Felipe Armijo <luis.armijo@axity.com>
2020-11-24 21:02:47 -0300

    crud rolservice

--- src/main/java/com/escalab/service/impl/RolServiceImpl.java
+++ src/main/java/com/escalab/service/impl/RolServiceImpl.java
@@ -1,4 +1,42 @@
 package com.escalab.service.impl;
 
-public class RolServiceImpl {
+import com.escalab.model.Rol;
+import com.escalab.repo.IRolRepo;
+import com.escalab.service.IRolService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RolServiceImpl implements IRolService {
+
+    @Autowired
+    private IRolRepo repo;
+
+    @Override
+    public Rol registrar(Rol obj) {
+        return repo.save(obj);
+    }
+
+    @Override
+    public Rol modificar(Rol obj) {
+        return repo.save(obj);
+    }
+
+    @Override
+    public List<Rol> listar() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Rol leerPorId(Integer id) {
+        Optional<Rol> op = repo.findById(id);
+        return op.isPresent() ? op.get() : new Rol();
+    }
+
+    @Override
+    public boolean eliminar(Integer id) {
+        repo.deleteById(id);
+        return true;
+    }
 }
