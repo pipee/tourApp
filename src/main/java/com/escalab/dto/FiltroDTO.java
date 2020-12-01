f06a9553c8d1787b0d81f4a363f8c3172d8606aa
Luis Felipe Armijo <luis.armijo@axity.com>
2020-12-01 17:38:59 -0300

    dto filtro busqueda nombres

---
+++ src/main/java/com/escalab/dto/FiltroDTO.java
@@ -0,0 +1,14 @@
+package com.escalab.dto;
+
+public class FiltroDTO {
+
+    private String nombreCompleto;
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+}
