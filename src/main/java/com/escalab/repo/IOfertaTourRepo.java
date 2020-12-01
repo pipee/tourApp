83a22293a71a9e391a186c0f9b59c066392bc91b
Luis Felipe Armijo <luis.armijo@axity.com>
2020-12-01 17:37:44 -0300

    repo oferta tour, busquedas

--- src/main/java/com/escalab/repo/IOfertaTourRepo.java
+++ src/main/java/com/escalab/repo/IOfertaTourRepo.java
@@ -2,6 +2,16 @@ package com.escalab.repo;
 
 import com.escalab.model.OfertaTour;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IOfertaTourRepo extends JpaRepository<OfertaTour, Integer> {
+
+    @Query("from OfertaTour ot where LOWER(ot.guia.nombres) like %:nombre% or LOWER(ot.guia.apellidos) like %:nombre%")
+    List<OfertaTour> listarTourPorNombreGuia(@Param("nombre") String nombre);
+
+    @Query("from OfertaTour ot where ot.guia.id =:id")
+    List<OfertaTour> listarTourPorIdGuia(@Param("id") Integer id);
 }
