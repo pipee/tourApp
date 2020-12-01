a8203ccc208832187f97f1ba29161b31b23d9609
Luis Felipe Armijo <luis.armijo@axity.com>
2020-12-01 18:41:18 -0300

    capa de servicio

--- src/main/java/com/escalab/AuthException.java
+++ src/main/java/com/escalab/AuthException.java
@@ -1,4 +1,33 @@
 package com.escalab;
 
-public class AuthException {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthException implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        final Map<String, Object> mapException = new HashMap<>();
+
+        mapException.put("error", "401");
+        mapException.put("message", "No estas autorizado para acceder a este recurso");
+        mapException.put("exception", "No Autorizado");
+        mapException.put("path", request.getServletPath());
+        mapException.put("timestamp", LocalDateTime.now());
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), mapException);
+    }
 }
